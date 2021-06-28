22e2be5bbb0af6cc9d965421946c636011ec94d0
Majd Hafiri <majd.hafiri@stud.th-deg.de>
2021-06-28 15:47:50 +0000

    Upload New File

---
+++ ex1_layer3_send.py
@@ -0,0 +1,24 @@
+#Import
+from time import sleep
+
+#Creating a udp message
+udpMessage = message_builder.create_udp_message()
+#setting the destination and source Ip addresses of the udp message to Loopback adapter IP address
+udpMessage.ipv4_header.ip_address_source = "169.254.247.134"
+udpMessage.ipv4_header.ip_address_destination = "169.254.247.134"
+#setting source and destination port numbers
+udpMessage.udp_header.port_source = 9999
+udpMessage.udp_header.port_destination = 10000
+
+#sending a message after every second for 20 seconds ( 20 messages )
+for i in range(20):
+    
+    if i < 10:
+        dataset = "0{0} 02 03 04 09".format(i)
+    else:
+        dataset = "{0} 02 03 04 09".format(i)
+              
+    udpMessage.payload = System.Array[Byte](bytearray.fromhex(dataset))
+    udpMessage.send()
+    
+    sleep(1)
