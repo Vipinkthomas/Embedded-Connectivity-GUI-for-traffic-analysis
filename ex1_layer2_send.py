ff7ae74f9570990c7bcb36a1393eacc0c7004438
Majd Hafiri <majd.hafiri@stud.th-deg.de>
2021-07-02 09:26:50 +0000

    Update ex1_layer2_send.py

--- ex1_layer2_send.py
+++ ex1_layer2_send.py
@@ -10,12 +10,15 @@ g_ethernet_msg.mac_address_destination = "02:00:4C:4F:4F:50"
 for i in range(20):
     #Loop which will change the first byte of the payload data and send a message every 1 second for 20 seconds
     if i < 10:
+        #Increasing the first byte of the payload data
         dataset = "0{0} 02 03 04 09".format(i)
     else:
         dataset = "{0} 02 03 04 09".format(i)
-              
+    
+    #Adding data to payload before sending the message
     g_ethernet_msg.payload = System.Array[Byte](bytearray.fromhex(dataset))
     g_ethernet_msg.send()
     
+    #time sleep for 1 second
     sleep(1)
 
